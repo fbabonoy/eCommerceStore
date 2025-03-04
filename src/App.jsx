366eb364b2c51515df6d30715c52464e40031d6b
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-03-03 21:47:23 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import CartData from './component/CartData'
 import ProductPage from './Page/ProductPage'
+import CartPage from './Page/CartPage'
 
 function App() {
 
   return (
     <>
     <CartData>
-
-    <ProductPage />
+      <Routes>
+        <Route path='/' element={<ProductPage />}/>
+        <Route path='/cart' element={<CartPage />}/>
+      </Routes>
     </CartData>
     </>
   )
